92f2d04f45973347ff81a7463bcbeebb0c03271b
O'Donald Hudson <odonaldhudson@gmail.com>
2020-10-07 12:21:43 -0500

    update

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -1,5 +1,9 @@
 package com.meritamerica.assignment1;
 
 public class MeritAmericaBankApp {
+	public static void main(String [] args) {
+		AccountHolder accountholder = new AccountHolder("O'Donald", "n/a", "Hudson", "02", 100.00, 100.00);
+		System.out.println(accountholder.toString());
+	}
 	
 }
